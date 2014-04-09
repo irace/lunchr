6b2713f9306fffd8c847d083a92286877ca1721f
Bryan Irace <bryan.irace@gmail.com>
2014-04-08 20:45:58 -0400

    Connect to API

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
-var express = require('express'),
-    request = require('request'),
-    querystring = require('querystring');
+var express = require('express')
+  , request = require('request')
+  , qs      = require('querystring')
+  , app     = express();
 
-//var app = express();
-//
-//var places;
-//
-//app.get('/random', function(req, res) {
-//
-//});
-//
-//app.get('/', function(req, res) {
-    var query = querystring.stringify({
-        client_id : 'IUWMBGIE1YVVYO3UDDJOJKVCIY3CTJ5HNDH4H5LSZAOHMXYT',
-        client_secret : 'GZYEDWLTAGEF3KGX3YJACULGF1GS0EZP1Q5MILZD1LWMW3FD',
-        v : '20131103'
-    });
+app.get('/random', function(req, res) {
+});
 
-//    request('https://api.foursquare.com/v2/users/2099890/lists?' + query, function (error, response, body) {
-//
-////        res.send('hello world');
-//    });
+app.get('/', function(req, res) {
 
-    var url = 'https://api.foursquare.com/v2/lists/5211139711d2019ea4f04f89?' + query;
+  var url = 'https://api.foursquare.com/v2/lists/5211139711d2019ea4f04f89?' + query;
 
-	request(url, function(error, response, body) {
-        console.log('');
-	});
-//});
-//
-//app.listen(3000, function() {
-//    console.log("Listening on port " + 3000);
-//});
+  res.send('hello world');
+});
+
+app.listen(3000, function() {
+  console.log("Listening on port " + 3000);
+
+  var query = qs.stringify({
+    client_id: 'IUWMBGIE1YVVYO3UDDJOJKVCIY3CTJ5HNDH4H5LSZAOHMXYT',
+    client_secret: 'A40SPXO1BF2CKOQPES5DIGW0VY2Z1FKTGJ4QX0BAXYOEJN4J',
+    v: '20131103'
+  });
+
+  request('https://api.foursquare.com/v2/lists/5211139711d2019ea4f04f89/?' + query, function (error, response, body) {
+    console.log(body);
+  });
+});
